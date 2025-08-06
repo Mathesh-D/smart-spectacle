ace92a76ce27543489b2daf8e3313ddeea614b0e
Mathesh D <145259408+Mathesh-D@users.noreply.github.com>
2025-08-06 17:24:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,10 +11,3 @@ os.makedirs(BARCODE_DIR, exist_ok=True)
 os.makedirs(LABEL_DIR, exist_ok=True)
 
 # Gemini 
-API_KEY = r"AIzaSyC7y43kotucX10_feqAywC--ajKrpix1cI"
-
-# Barcode Lookup API Key
-BARCODE_LOOKUP_API_KEY = r"mn5uqp6vlaq4teh4m7d7a4blica1ra"
-
-# MongoDB Connection
-MONGO_URI = "mongodb+srv://monish:OECpsWYv@inventory.3tm2w.mongodb.net/?retryWrites=true&w=majority&appName=Inventory"
